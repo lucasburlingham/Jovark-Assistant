9fe508b3d51916d91f9f4136376b020be4788506
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2019-10-14 08:55:41 -0400

    Add files via upload

---
+++ Java Source/Jovark-Java.java
@@ -0,0 +1,18 @@
+public static void entry(String[] args)
+{
+import java.util.Scanner;  // Import the Scanner class
+Scanner currentCommand = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+	system.out.println(">>$J:")
+}
+
+switch(currentCommand)
+{
+case "Hi"
+	system.out.println("Hi, what can I do for you today?")
+	break;
+case "man"
+	system.out.println("Greetings, man.")
+default 
+	system.out.println("Invalid Command. Please use the 'man' command to see a list of all avaliable commands.")
+	break;
