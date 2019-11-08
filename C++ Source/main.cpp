fe91ed4c12d551e0403588888c22bda9328f60f2
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2019-11-08 14:33:14 -0500

    add c++ file

---
+++ C++ Source/main.cpp
@@ -0,0 +1,27 @@
+#include string;
+	using namespace std;
+int main();
+{
+	start();
+	entry();	
+}
+	
+int start();
+{
+	cout << "Welcome to the Jovark Assistant. Enter your commands at the J$ prompt below:";
+}	
+	
+	
+	
+	
+	
+int email ();
+{
+	string messageRecipient;
+	string messageSubject;
+	string messageBody;
+	cin << "Enter recipient: " == messageRecipient;
+	cin << "Enter subject: " == messageSubject;
+	cin << "Enter message: " == messageBody;
+	cout << "Would you like to send your message, which reads as: ", "To: ", messageRecipient, "Subject: ", messageSubject, "Body: ", messageBody;
+}
